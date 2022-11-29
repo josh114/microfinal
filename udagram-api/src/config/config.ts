8e6571f58513dc302e4b42bf145c50b65de20cc2
josh114 <freejosh02@gmail.com>
2022-11-29 23:49:41 +0100

    update code base

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
-  'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
-  'jwt': {
-    'secret': process.env.JWT_SECRET,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'postgres',
+  host: 'postgres.cq9cqnm8lf1v.us-east-1.rds.amazonaws.com',
+  dialect: 'postgres',
+  aws_region: 'us-east-1',
+  aws_profile: 'default',
+  aws_media_bucket: 'bucket-2053815-dev',
+  url: 'http://localhost:8100',
+  jwt: {
+    secret: 'testing',
   },
 };
